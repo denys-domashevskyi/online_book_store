66ca2ac0c8b5618c648aa4dd28e3306bed3eb478
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2026-01-15 19:58:18 +0200

    Add swagger for registration

--- src/main/java/online/book/store/controller/AuthenticationController.java
+++ src/main/java/online/book/store/controller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package online.book.store.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import online.book.store.dto.UserRegistrationRequestDto;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User authentication", description = "Endpoints for user authentication")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/auth")
@@ -18,6 +21,8 @@ public class AuthenticationController {
     private final UserService userService;
 
     @PostMapping(value = "/registration")
+    @Operation(summary = "Register user",
+            description = "Register user")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
